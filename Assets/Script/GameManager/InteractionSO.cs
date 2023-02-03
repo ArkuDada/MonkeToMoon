64e7067d75d2e766ae0dccc06cbe8d0259b90633
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2023-02-04 00:30:11 +0700

    Interaction SO

--- Assets/Script/GameManager/InteractionSO.cs
+++ Assets/Script/GameManager/InteractionSO.cs
@@ -1,9 +1,75 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+
 
 [CreateAssetMenu(fileName = "InteractionSO", menuName = "ScriptableObjects/Interaction")]
 public class InteractionSO : ScriptableObject
 {
-    public List<ItemDataSO> items;
+    [SerializeField]
+    public InteractionRequirement Requirement;
     public Era era;
+    
+    public bool isResultItem;
+    public ItemType ResultItem;
+    public int ResultAmount;
+
+    void Interact()
+    {
+        if(Requirement.IsAble())
+        {
+            foreach(var r in Requirement.Requirements)
+            {
+                InventoryManager.Instance.RemoveItem(r.Item,r.Amount);
+
+            }
+            
+            if(isResultItem)
+            {
+                InventoryManager.Instance.AddItem(ResultItem, ResultAmount);
+
+            }
+            else
+            {
+                //upgrade landmark
+            }
+        }
+     
+    }
+}
+
+[Serializable]
+public class InteractionRequirement
+{
+    [SerializeField]
+    public List<Requirement> Requirements = new List<Requirement>();
+
+    //Imprement in object level
+    public bool IsAble()
+    {
+        if(Requirements.Count == 0)
+        {
+            return true;
+        }
+        
+        bool isAble = true;
+
+        foreach(var r in Requirements)
+        {
+            if(!InventoryManager.Instance.HaveItems(r.Item, r.Amount))
+            {
+                isAble = false;
+                break;
+            }
+        }
+        return isAble;
+    }
+}
+
+[Serializable]
+public struct Requirement
+{
+    public ItemType Item;
+    public int Amount;
 }
