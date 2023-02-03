2204b5688668e912d04a7dc61c8b7813fdb52140
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2023-02-04 02:49:08 +0700

    Update Scene with UI mock

--- Assets/Script/ItemInventory/InventoryManager.cs
+++ Assets/Script/ItemInventory/InventoryManager.cs
@@ -7,6 +7,7 @@ public class InventoryManager : MonoSingleton<InventoryManager>
 {
     private Dictionary<ItemType, int> Inv = new Dictionary<ItemType, int>();
 
+    [HideInInspector]
     public UnityEvent<Dictionary<ItemType, int>> OnInvUpdate = new UnityEvent<Dictionary<ItemType, int>>();
 
     private void UpdateInv()
