e71ed8b05c28df4b3aa35d68a8dc61716ebfc2c8
RealssT7 <61772105+RealssT7@users.noreply.github.com>
2023-02-04 10:31:16 +0700

    Item Data
    Item Data

--- Assets/Script/ItemInventory/ItemDataSO.cs
+++ Assets/Script/ItemInventory/ItemDataSO.cs
@@ -8,10 +8,35 @@ public class ItemDataSO : ScriptableObject
     public Sprite sprite;
 }
 
-public enum ItemType{
+public enum ItemType
+{
     NormalItem = 0,
-    Wood,
+    Fiber,
+    Rope,
+    Cloth,
     Stone,
+    Stone_Block,
+    Rope_Ladder,
+    Wood,
+    Wood_Plank,
+    Coal,
+    Torch,
+    Bronze_Ore,
+    Bronze_Bar,
+    Copper_Ore,
+    Copper_Bar,
+    Iron_Ore,
+    Iron_Bar,
+    Gold_Ore,
+    Gold_Bar,
+    Magnet_Ore,
+    Fuel,
+    Engine,
+    Battery,
     KeyItem = 99,
-    Tool,
+    Bronze_Pickaxe,
+    Bronze_Axe,
+    Copper_Pickaxe,
+    Copper_Axe,
+    Meteor_Ore,
 }
