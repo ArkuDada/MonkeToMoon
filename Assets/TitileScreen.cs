f11773796615e4aea64cfe16ce33f07e078fbf6f
ball <sippapard@hotmail.com>
2023-02-05 13:16:21 +0700

    TITLR

---
+++ Assets/TitileScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitileScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+    
+        SceneManager.LoadScene(1);
+    }
+public GameObject credits;
+    public void Credits()
+    {
+        credits.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
